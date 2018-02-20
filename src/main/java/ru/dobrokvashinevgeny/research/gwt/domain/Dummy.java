71de3e946e96ceec9afc6f10855165610eb5bef1
stalker <jenya_d1@mail.ru>
2018-02-20 21:57:21 +0300

    Initial commit

---
+++ src/main/java/ru/dobrokvashinevgeny/research/gwt/domain/Dummy.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2018 Tander, All Rights Reserved.
+ */
+
+package ru.dobrokvashinevgeny.research.gwt.domain;
+
+/**
+ * Класс Dummy
+ */
+public class Dummy {
+}
---
+++ src/main/java/ru/dobrokvashinevgeny/research/gwt/infrastructure/persistence/Dummy.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2018 Tander, All Rights Reserved.
+ */
+
+package ru.dobrokvashinevgeny.research.gwt.infrastructure.persistence;
+
+/**
+ * Класс Dummy
+ */
+public class Dummy {
+}
